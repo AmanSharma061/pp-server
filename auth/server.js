e79bda9f42f995273877117188ca4ba5baae1b4f
Aman <b121061@iiit-bh.ac.in>
2024-02-15 00:10:40 +0530

    final

--- auth/server.js
+++ auth/server.js
@@ -42,7 +42,7 @@ router.post("/api/login", async (req, res) => {
     const emailExists = await User.findOne({ email: email });
 
     if (!emailExists) {
-      return res.status(500).send({ error: "Invalid Credentials" });
+      return res.status(401).json({ error: "Invalid Credentials" });
     } else {
       const pMatch = await bcrypt.compare(password, emailExists.password);
       if (pMatch) {
